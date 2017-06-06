a210bee90d8d3ee7e4c27b1209ce8e23c5ef90e0
JohnLui <wenhanlv@gmail.com>
2017-06-06 20:51:25 +0800

    login page bug fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,9 +54,9 @@
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{-- <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
-                                </a>
+                                </a> --}}
                             </div>
                         </div>
                     </form>
